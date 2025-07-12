15e9e8db9e812e9310aff7164e68d867ae20ccc1
Vidit Khandelwal <vkhandelwal48@gmail.com>
2025-07-12 10:16:38 +0530

    getting started with Typescript

---
+++ typescript.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+var result = add(5, 10);
+console.log("The result is: ".concat(result));
---
+++ typescript.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const result = add(5, 10);
+console.log(`The result is: ${result}`);
