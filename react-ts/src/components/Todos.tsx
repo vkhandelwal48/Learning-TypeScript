f1d682eedb20c0753bb8ab5c1b3d03cbabf8b84e
Vidit Khandelwal <vkhandelwal48@gmail.com>
2025-07-13 13:58:51 +0530

    adding TodoItem List Component

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
@@ -6,7 +7,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
         <ul>
             {props.items.map(
                 (item) => (
-                    <li key={item.id}>{item.text}</li>
+                    <TodoItem key={item.id} text={item.text} />
                 )
             )}
         </ul>
