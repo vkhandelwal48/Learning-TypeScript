11a122167f8665c01821fc4886682e40100e391f
Vidit Khandelwal <vkhandelwal48@gmail.com>
2025-07-13 21:42:02 +0530

    remove todos functionality

--- react-ts/src/components/TodoItem.tsx
+++ react-ts/src/components/TodoItem.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
+const TodoItem: React.FC<{ text: string, onRemoveTodo:() => void }> = (props) => {
     return (
-        <li className={classes.item}>{props.text}</li>
+        <li className={classes.item} onClick={props.onRemoveTodo}>{props.text}</li>
     )
 }
 
